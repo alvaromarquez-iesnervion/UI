667f2d365a49d196c76d932bbef4096ff4db1d0f
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2025-11-26 14:39:41 +0100

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Persona
+    {
+        public int Id { get; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        Persona(int id, string nombre, string apellidos, DateTime fechaNacimiento, string direccion, string telefono)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellidos = apellidos;
+            FechaNacimiento = fechaNacimiento;
+            Direccion = direccion;
+            Telefono = telefono;
+        }
+
+        
+    }
+}
